f0c21012e5addf78a4c6d33d7a1f8ef7ee14a01b
klassegeljakt <klasseg@kth.se>
2017-11-20 13:33:31 +0100

    Added map macro

--- multiop/examples.c
+++ multiop/examples.c
@@ -9,20 +9,48 @@
 #include <stdio.h>
 /*****************************************************************************/
 
+double square(double x) {
+  return x*x;
+}
+
 int main(const int argc, const char *argv[]) {
-  int x    = 0;
-  float y  = 1;
+  int    x = 0;
+  float  y = 1;
   double z = 2;
   long   l = 100;
 
   /* Add 3 to x, y and z */
   printf("   (%d, %f, %g) += 3\n", x,y,z);
+
   add (x,y,z) += 3;
+
   printf("=> (%d, %f, %g)\n\n", x,y,z);
+
+
 
   /* Multiply 3 to x, y and z*/
   printf("   (%d, %f, %g, %ld) *= 4\n", x,y,z,l);
+
   mul (x,y,z,l) *= 3;
+
+  printf("=> (%d, %f, %g, %ld)\n", x,y,z,l);
+
+
+
+  /* Map each element x to x^2 */
+  printf("   (%d, %f, %g, %ld) ^2\n", x,y,z,l);
+
+  map (x,y,z,l) = square;
+
+  printf("=> (%d, %f, %g, %ld)\n", x,y,z,l);
+
+
+
+  /* "=" can be replaced with "to" if it makes more sense */
+  printf("   (%d, %f, %g, %ld) ^2\n", x,y,z,l);
+
+  map (x,y,z,l) with square;
+
   printf("=> (%d, %f, %g, %ld)\n", x,y,z,l);
 
   return 0;
--- multiop/multiop.h
+++ multiop/multiop.h
@@ -19,7 +19,6 @@
 
 #define MOA(...) MOA_(__VA_ARGS__,MOA3,MOA2,MOA1,MOA0)(__VA_ARGS__)
 
-
 /* Multiply to multiple variables */
 
 #define MOM3(_0,_1,_2,_3) _0 *= diff; _1 *= diff; _2 *= diff; _3 *= diff;
@@ -31,7 +30,23 @@
 
 #define MOM(...) MOM_(__VA_ARGS__,MOM3,MOM2,MOM1,MOM0)(__VA_ARGS__)
 
+/* Map multiple variables */
+
+#define MOF3(_0,_1,_2,_3) _0 = mf(_0); _1 = mf(_1); _2 = mf(_2); _3 = mf(_3);
+#define MOF2(_0,_1,_2)    _0 = mf(_0); _1 = mf(_1); _2 = mf(_2);
+#define MOF1(_0,_1)       _0 = mf(_0); _1 = mf(_1);
+#define MOF0(_0)          _0 = mf(_0);
+
+#define MOF_(_0,_1,_2,_3,MOF_MACRO,...) MOF_MACRO
+
+#define MOF(...) MOF_(__VA_ARGS__,MOF3,MOF2,MOF1,MOF0)(__VA_ARGS__)
+
 /* ============================== API MACROS =============================== */
+
+
+typedef double(*map_f)(double);
+
+#define with =
 
 /* Add to multiple variables at once */
 
@@ -52,3 +67,11 @@
         MOM(__VA_ARGS__)                                                      \
       } else                                                                  \
         diff
+
+#define map(...)                                                              \
+  for(int mi = 0; !mi;)                                                       \
+    for(map_f mf; mi < 2; mi++)                                               \
+      if(mi) {                                                                \
+        MOF(__VA_ARGS__)                                                      \
+      } else                                                                  \
+        mf
